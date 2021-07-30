740fc72b0c4cceebc78b3c6c09df5aa46c2d1759
Yemifino <steepansteepan@gmail.com>
2021-07-30 10:24:47 +0200

    splash screen

--- app/src/main/java/jok/erf/mtucischedule/MainActivity.kt
+++ app/src/main/java/jok/erf/mtucischedule/MainActivity.kt
@@ -1,16 +1,31 @@
 package jok.erf.mtucischedule
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
 
-        // Write a message to the database
+        },3000)
+
+       /* var button: Button = findViewById(R.id.button_go);
+        val intent = Intent(this,HellowActivity::class.java)
+        button.setOnClickListener {startActivity(intent) }
+         Write a message to the database */
         val database = Firebase.database
         val myRef = database.getReference("message")
 
